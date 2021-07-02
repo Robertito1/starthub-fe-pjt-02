561b51b3b50f4eecdc4cc8441cd2ec4e90687136
Robert <orazurobert@yahoo.com>
2021-07-02 23:32:02 +0100

    basic login functionality

--- Login/script.js
+++ Login/script.js
@@ -1,8 +1,26 @@
-function myFunction() {
-    var x = document.getElementById("myInput");
-    if (x.type === "password") {
-        x.type = "text";
+const form = document.getElementById("loginForm");
+
+function showPassword() {
+    var passwordInput = document.getElementById("passwordValue");
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
     } else {
-        x.type = "password";
+        passwordInput.type = "password";
     }
 }
+
+const login = (e) => {
+    e.preventDefault()
+    axios.post('http://localhost:3000/api/v1/auth/sign-in', {
+            email: e.target.email.value,
+            password: e.target.password.value
+        })
+        .then(function(response) {
+            window.location.href = "http://127.0.0.1:5500/Landing%20page/index.html";
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+}
+
+form.addEventListener('submit', login);
